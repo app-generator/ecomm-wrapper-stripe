44c2033a7cd0ac29051438df951c8ee710ef1c98
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-18 12:19:14 +0300

    Added runner

---
+++ run.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+import os
+from dotenv import load_dotenv
+
+from py_stripe import get_products
+
+load_dotenv()
+
+if __name__ == "__main__":
+
+    STRIPE_API_KEY = os.getenv('STRIPE_API_KEY', None)
+
+    if STRIPE_API_KEY:
+        get_products( STRIPE_API_KEY )
+    else:
+        print( 'Err: STRIPE_API_KEY not provided in ENV' )
