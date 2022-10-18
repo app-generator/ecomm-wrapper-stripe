3c8bb2c36d31150fc813f24c7956c978de67f586
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-18 12:17:07 +0300

    CleanUp

--- getstripeproducts/__init__.py
+++
@@ -1,1 +0,0 @@
-from . stripeproducts import get_products
--- testproduct.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-from getstripeproducts import get_products
-load_dotenv()
-
-load_dotenv()
-
-get_products(os.getenv('STRIPE_API_KEY'))
