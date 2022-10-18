0a27dee004317111013e14804a5fb2709aee4b32
App Generator <51070104+app-generator@users.noreply.github.com>
2022-10-18 16:48:26 +0300

    Improve the saved file

--- stripe_python/util.py
+++ stripe_python/util.py
@@ -14,20 +14,30 @@ def get_products( stripeApiKEY, outputFile='products.json'):
     productdict = []
     for product in products:
         dict= {}
-        dict['id'          ] = product['id']
-        dict['Name'        ] = product['name']
-        dict['Description' ] = product['description']
-        dict['Images'      ] = product['images']
-        dict['Price_Default'       ] = { product["default_price"]["id"]: product["default_price"]["unit_amount"]/100}
+        dict['id'            ] = product['id']
+        dict['name'          ] = product['name']
+        dict['description'   ] = product['description']
+        dict['images'        ] = product['images']
+
+        # Get default price 
+        dict['price_default'  ] = { product["default_price"]["id"]: product["default_price"]["unit_amount"]/100}
+        
+        # Pull all prices 
         all_prices = stripe.Price.list(product=product["id"]).data
         pricedict = {}
 
+        # Save all prices
         for price in all_prices:
             pricedict[price["id"]] = price["unit_amount"] / 100
 
-        dict['Prices'] = pricedict
+        dict['prices'] = pricedict
         productdict.append(dict)
 
     with open(outputFile, "w") as outfile:
-        json.dump({"data": productdict}, outfile)
 
+        # json.dump({"data": productdict}, outfile)
+
+        products = json.dumps( {"data": productdict}, indent=4, separators=(',', ': ') )        
+
+        outfile.write( products )
+        outfile.close()
